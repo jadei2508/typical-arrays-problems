2b1240d7233adf9eeb68129dd632a743a5fb8b5b
Roman Alikevich <roma.alikevich@mail.ru>
2020-10-01 12:50:12 +0300

    feat: add functionality for function min

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+    let length = array.length;
+    if(length === 0) {
+        return 0;
+    }
+    let min = array[0];
+    let i = 1;
+    while(i < length - 1) {
+        if(min > array[i]) {
+            min = array[i];
+        }
+        i++;
+    }
+  return min;
 }
 
 exports.max = function max (array) {
