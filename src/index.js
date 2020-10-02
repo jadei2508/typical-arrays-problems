099095a219b82463f0688463d3f982d0c158ddbd
Roman Alikevich <roma.alikevich@mail.ru>
2020-10-02 08:42:39 +0300

    fix: implement checking for missing incoming parameters

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 
 exports.min = function min (array) {
-    let length = array.length;
-    if(length === 0) {
+    if(array === undefined || array.length === 0) {
         return 0;
     }
+    let length = array.length;
     let min = array[0];
     let i = 1;
-    while(i < length - 1) {
+    while(i < length) {
         if(min > array[i]) {
             min = array[i];
         }
         i++;
     }
-  return min;
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return 0;
 }
 
 exports.avg = function avg (array) {
